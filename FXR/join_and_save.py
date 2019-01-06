5dfd9dc2f01d20ef1845034110df44a7afd609df
John Hawkins <john.hawkins@john.hawkins.MacBook>
2019-01-06 17:04:33 +1100

    FXR Processed File

--- FXR/join_and_save.py
+++ FXR/join_and_save.py
@@ -1,27 +1,26 @@
 import pandas as pd
 import numpy as np
 
-df1 = pd.read_csv('FXR.csv')
+df1 = pd.read_csv('FXR1.csv')
 df2 = pd.read_csv('FXR2.csv')
 
-df1['Date'] = pd.to_datetime(df1['Date'], format='%m/%d/%Y')
-df2['Date'] = pd.to_datetime(df2['Date'], format='%m/%d/%Y')
+df1['DATE'] = pd.to_datetime(df1['DATE'], format='%d-%b-%Y')
+df2['DATE'] = pd.to_datetime(df2['DATE'], format='%d-%b-%Y')
 
 # CREATE A NEW DATAFRAME WITH ALL DATES
-maxdate = max(df2['Date'])
-mindate = min(df1['Date'])
+maxdate = max(df2['DATE'])
+mindate = min(df1['DATE'])
 
 mydates = pd.date_range(mindate, maxdate)
 
 newdf = pd.DataFrame()
 
 for j in range(len(mydates)):
-    newdf = newdf.append({'Date': mydates[j]}, ignore_index=True)
+    newdf = newdf.append({'DATE': mydates[j]}, ignore_index=True)
 
-new_df = pd.merge(newdf, df,  how='left', left_on=['Department','Date'], right_on = ['Department','Date'])
+join_df = pd.concat([df1,df2], ignore_index=True)
 
+new_df = pd.merge(newdf, join_df,  how='left', left_on=['DATE'], right_on = ['DATE'])
 
-
-
-
+new_df.to_csv('FXR_Joined_Processed.csv', sep=',', encoding='utf-8', index=False, header=True)
 
